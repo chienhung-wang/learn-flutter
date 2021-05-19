050627a3ec07526e8e22af41c91bb05fe0312c17
Chien-Hung Wang <78210692+chienhung-wang@users.noreply.github.com>
2021-05-19 19:23:08 +0800

    Added skeleton for dice app

---
+++ dicee-flutter-master/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/dz/Developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/dz/Documents/Technologies/learn-flutter/dicee-flutter-master"
+export "FLUTTER_TARGET=/Users/dz/Documents/Technologies/learn-flutter/dicee-flutter-master/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/dz/Documents/Technologies/learn-flutter/dicee-flutter-master/.dart_tool/package_config.json"
